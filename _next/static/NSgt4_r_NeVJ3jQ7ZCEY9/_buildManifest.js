5f9c1b5919d1b944bb48ac1143d19c1af294b5b5
Daniel Mafra <danielmafradev@gmail.com>
2021-11-22 14:51:26 -0300

    update paths

---
+++ _next/static/NSgt4_r_NeVJ3jQ7ZCEY9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/047f06674e534ad9.css","static/chunks/pages/index-0549a96183aa9ad9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
